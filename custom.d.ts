f0d9399fdd49aae7eed88b5cd3a174cae38c6357
Erik Töger <erik.toger@gmail.com>
2022-07-17 16:53:45 +0200

    Change types for images

--- custom.d.ts
+++ custom.d.ts
@@ -1,4 +1,16 @@
-declare module "*.svg";
-declare module "*.jpg";
-declare module "*.png";
-declare module "*.ico";
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
+declare module "*.jpg" {
+  const src: string;
+  export default src;
+}
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
+declare module "*.ico" {
+  const src: string;
+  export default src;
+}
